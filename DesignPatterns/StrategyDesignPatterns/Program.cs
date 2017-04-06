b6f367f3a3c00493022b00fc352a03d1a4bd2aec
Thanh Doan <t.doan@xlntelecom.co.uk>
2017-04-06 15:38:09 +0100

    Strategy Implementation first

--- DesignPatterns/StrategyDesignPatterns/Program.cs
+++ DesignPatterns/StrategyDesignPatterns/Program.cs
@@ -1,9 +1,70 @@
-﻿namespace StrategyDesignPatterns
+﻿using System;
+
+namespace StrategyDesignPatterns
 {
     internal class Program
     {
         static void Main()
         {
+            Context context = new Context();
+            context.SwitchStrategy();
+            Random r = new Random(37);
+            for (int i = Context.Start; i < Context.Start + 15; i++)
+            {
+                if (r.Next(3) == 2)
+                {
+                    Console.Write("|| ");
+                    context.SwitchStrategy();
+                }
+                Console.Write(context.Algorighm()+ " ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+
+
+    internal class Context
+    {
+        public const int Start = 5;
+        public int Counter = 5;
+
+        private IStrategy strategy = new Strategy1();
+
+        public int Algorighm()
+        {
+            return strategy.Move(this);
+        }
+
+        public void SwitchStrategy()
+        {
+            if(strategy is Strategy1)
+                strategy = new Strategy2();
+            else
+            {
+                strategy = new Strategy1();
+            }
+        }
+    }
+
+    interface IStrategy
+    {
+        int Move(Context c);
+    }
+
+    internal class Strategy1 : IStrategy
+    {
+        public int Move(Context c)
+        {
+            return ++c.Counter;
+        }
+    }
+
+    internal class Strategy2 : IStrategy
+    {
+        public int Move(Context c)
+        {
+            return --c.Counter;
         }
     }
 }
